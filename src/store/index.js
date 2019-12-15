84438f1f8906c7e2c291818d9a8f0dbecf73eba6
Noemi Millman <noemi@triopter.com>
2019-12-15 13:56:06 -0500

    skeleton of the app I want to build

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import {
+  createStore,
+  compose,
+  applyMiddleware,
+} from "redux";
+import thunk from "redux-thunk";
+import reducers from "./reducers";
+
+const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducers,
+  storeEnhancers(applyMiddleware(thunk))
+);
+
+export default store;
---
+++ src/store/reducers.js
@@ -0,0 +1,14 @@
+const initialState = {
+  teams: [],
+}
+
+const reducers = (state = initialState, action) => {
+  switch (action.type) {
+    default: {
+      return state
+    }
+  }
+}
+
+export default reducers;
+
