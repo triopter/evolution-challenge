7c520c655966ec5fd4d6b324538296226c1fe869
Noemi Millman <noemi@triopter.com>
2019-12-15 15:17:07 -0500

    initial models / DB schema

--- football/models.py
+++ football/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
-# Create your models here.
+LEAGUES = [
+    ("English Premier League", "English Premier League",),
+    ("La Liga", "La Liga",),
+    ("Bundesliga", "Bundesliga",),
+]
+
+
+class Season(models.Model):
+    years = models.CharField(max_length=191, unique=True)
+
+    def __str__(self):
+        return self.years
+
+
+class Team(models.Model):
+    league = models.CharField(max_length=191, choices=LEAGUES, blank=True)
+    name = models.CharField(
+        max_length=191
+    )  # 191 because if we wanted to port this to MySQL,
+    # that would be the max width of a UTF8MB4 column
+    # not that we should really ever need 191
+    key = models.CharField(max_length=191)
+    code = models.CharField(max_length=3, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Match(models.Model):
+    season = models.ForeignKey(Season, related_name="matches", on_delete=models.CASCADE)
+    # we could use a FK to round and store the round name, but it's not very useful and
+    # for current purposes only creates an extra layer of indirection
+    round_number = models.PositiveSmallIntegerField()
+    match_date = models.DateField()
+
+    def __str__(self):
+        # Needing a FK to fetch the name is ugly, but our data set is small enough
+        # to make it viable.  If this were done frequently in production it might
+        # necessitate denormalization or using a less friendly string representation.
+        team_names = [s.team.name for s in self.scores.order_by("is_home_team")]
+        vs = " vs. ".join(team_names)
+        return f"{vs} ({self.season.years})"
+
+
+class Score(models.Model):
+    match = models.ForeignKey(Match, related_name="scores", on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, related_name="scores", on_delete=models.CASCADE)
+    score = models.SmallIntegerField()
+    is_home_team = models.BooleanField(
+        help_text="We're pretending Team 2 is always the home team"
+    )
+
+    def __str__(self):
+        return f"{self.team}: {self.score} ({self.match.match_date})"
